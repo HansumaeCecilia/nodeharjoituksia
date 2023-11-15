1dad11c289ce9435de527abfc230d60b170a2d88
Cecilia <48033@edu.raseko.fi>
2023-11-15 14:19:43 +0200

    Weather data in to db

--- app.js
+++ app.js
@@ -7,10 +7,11 @@
 // Use Express as web engine
 const express = require('express');
 // Use Express Handlebars as template engine
-const {engine} = require('express-handlebars');
+const { engine } = require('express-handlebars');
 
 // Module to run queries
 const query = require('./getPGData');
+
 
 // EXPRESS APPLICATION SETTINGS
 // ----------------------------
@@ -30,76 +31,84 @@ app.set('view engine', 'handlebars');
 // URL ROUTES
 // ----------
 
-//TODO: Add date and time as dynamic data for the homepage, is it sensible to use server for creating time values?
 
-// Route to homepage
+// Route to home page 
 app.get('/', (req, res) => {
-    
-    // Handlebars needs a key to show data on a web page, json is a good way to send it
+
+    // Handlebars needs a key to show data on a page, json is a good way to send it
+    // Create key structure and reset values
     let homePageData = {
         'price': 0,
         'wind': 0,
-        'temperature': 18
-    }
+        'temperature': 0
+    };
 
-    cprice.getCurrentPrice().then((resultset) => {
-        console.log.apply(resultset.rows[0])
+    // Define a query to get current price from the DB
+    const currentPriceQuery = 'SELECT price FROM public.current_prices';
+    query.getSqlData(currentPriceQuery).then((resultset) => {
+
+        // Set the price value according to the query (1st row, column price)
         homePageData.price = resultset.rows[0]['price']
+
+        // Render index.handlebars and send dynamic data to the page
+        res.render('index', homePageData)
     })
-
-    res.render('index', homePageData)
-
 });
 
 // Route to hourly data page
 app.get('/hourly', (req, res) => {
 
     // Data will be presented in a table. To loop all rows we need a key for table and for column data
-    cpriceTable.getCurrentPriceTable().then((resultset) => {
+    const priceTableQuery = 'SELECT * FROM public.hourly_page'
+    query.getSqlData(priceTableQuery).then((resultset) => {
         let tableData = resultset.rows
         let hourlyPageData = {
             'tableData': tableData
         };
-        console.log(hourlyPageData)
-        res.render('hourly', hourlyPageData)
+        res.render('hourly', hourlyPageData);
     })
-    
+
 });
 
 // Route to hourly chart page graph.handlebars
 app.get('/graph', (req, res) => {
-    cpriceChart.getCurrentPriceChartData().then((resultset) => {
-        let xyData = resultset.rows
+
+    const chartQuery = 'SELECT hour, price FROM public.hourly_page';
+    query.getSqlData(chartQuery).then((resultset) => {
+        let xyData = resultset.rows;
 
         // Create empty arrays for x-axis and y-axis data
-        let xData = []
-        let yData = []
+        let xData = [];
+        let yData = [];
 
         // Add values to those arrays in a loop
         for (i in xyData) {
             let xvalueStr = xyData[i]['hour'];
 
             // Time valuest must be converted to numbers for the chart to render
-            let xvalue = Number(xvalueStr)
-            xData.push(xvalue)
+            let xvalue = Number(xvalueStr);
+            xData.push(xvalue);
 
             // Price values are numbers so no need to convert
             let yvalue = xyData[i]['price'];
-            yData.push(yvalue)
+            yData.push(yvalue);
         }
         // Data will be presented in a bar chart. Data will be sent as JSON array
-        xData = JSON.stringify(xData)
-        yData = JSON.stringify(yData)
+        xData = JSON.stringify(xData);
+        yData = JSON.stringify(yData);
 
         // X-axis values are called hours and y-axis values prices in the handlebars file
         let chartPageData = { 'hours': xData, 'prices': yData };
 
         // Render and send dynamic data to the page 
-        res.render('graph', chartPageData)
+        res.render('graph', chartPageData);
     });
 
 
 });
+
+// TODO: Add route to forecasts page
+
 
 // START THE LISTENER
 app.listen(PORT);
